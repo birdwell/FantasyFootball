baf41cf88c48e540cf1555ed10ad62d496170938
Josh Birdwell <jbirdwell34@gmail.com>
2015-10-01 13:44:22 -0500

    add profile pictures, show more players, add styling

--- app/ranking/ranking.js
+++ app/ranking/ranking.js
@@ -7,7 +7,7 @@
     $scope.user = user;
     $scope.FBURL = FBURL;
     var playersRef = new Firebase(FBURL).child("players");
-    var query = playersRef.limitToFirst(20);
+    var query = playersRef.limitToFirst(400);
 
     var players = $firebaseArray(query);
 
